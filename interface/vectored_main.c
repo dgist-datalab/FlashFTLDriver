cb4b06861e53c9c4627a90f62da32664d36d01a5
kukania <angkswnstn@naver.com>
2020-09-14 20:01:12 +0900

    change gc_logic

--- interface/vectored_main.c
+++ interface/vectored_main.c
@@ -30,7 +30,7 @@ int main(int argc,char* argv[]){
 	inf_init(0,0,argc,argv);
 	bench_init();
 	bench_vectored_configure();
-//	bench_add(VECTOREDRSET,0,RANGE,RANGE);
+	bench_add(VECTOREDRSET,0,RANGE,RANGE);
 	bench_add(VECTOREDRW,0,RANGE,RANGE*2);
 	//bench_add(VECTOREDRGET,0,RANGE/100*99,RANGE/100*99);
 	printf("range: %lu!\n",RANGE);
