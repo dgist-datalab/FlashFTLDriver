3da52006adf28211f2fd3a5c1c24bfe3d30b9cef
kukania <angkswnstn@gmail.com>
2021-06-03 19:56:20 +0900

    temp commit before edit AMF

--- Makefile
+++ Makefile
@@ -18,7 +18,7 @@ DEBUGFLAGS=\
 			-rdynamic\
 			-Wno-pointer-arith\
 			-g\
-#-fsanitize=address\
+-fsanitize=address\
 
 export COMMONFLAGS=\
 			-Wno-write-strings\
@@ -29,8 +29,8 @@ export COMMONFLAGS=\
 			-DSLC\
 			-D$(TARGET_BM)\
 			-Wno-unused-but-set-variable\
-			-DDEBUG\
-	-O3 -march=native -mtune=native -flto=20 \
+			-DLSM_DEBUG\
+#	-O3 -march=native -mtune=native -flto=20 \
 
 COMMONFLAGS+=$(DEBUGFLAGS)\
 
--- interface/vectored_main.c
+++ interface/vectored_main.c
@@ -42,13 +42,13 @@ int main(int argc,char* argv[]){
 		inf_init(0,0,argc,argv, false);
 		bench_init();
 		bench_vectored_configure();
-		//bench_add(VECTOREDSSET,0,RANGE,RANGE);
-		//bench_add(VECTOREDSGET,0,RANGE,RANGE);
+	//	bench_add(VECTOREDSSET,0,RANGE,RANGE);
+	//	bench_add(VECTOREDSGET,0,RANGE,RANGE);
 		//bench_add(VECTOREDRSET,0,RANGE,RANGE*3);
 		//bench_add(VECTOREDRSET,0,RANGE/2,RANGE/2);
-		//bench_add(VECTOREDRW,0,RANGE/2/100*99,RANGE/100*99);
-		//bench_add(VECTOREDRW,0,RANGE,RANGE*4);
-		inf_algorithm_testing();
+		//bench_add(VECTOREDRW,0,RANGE,RANGE/2);
+		bench_add(VECTOREDRW,0,RANGE,RANGE*8);
+		//inf_algorithm_testing();
 	}
 	printf("range: %lu!\n",RANGE);
 
