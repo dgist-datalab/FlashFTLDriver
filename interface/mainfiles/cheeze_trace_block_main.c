c848abeb80ea301761d5f4638670036326a9dd9d
kukania <angkswnstn@gmail.com>
2021-07-19 20:49:25 +0900

    add trace_block_main

---
+++ interface/mainfiles/cheeze_trace_block_main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+#include <signal.h>
+#include "../../include/FS.h"
+#include "../../include/settings.h"
+#include "../../include/types.h"
+#include "../../bench/bench.h"
+#include "../interface.h"
+#include "../vectored_interface.h"
+#include "../cheeze_hg_block.h"
+
+void log_print(int sig){
+	free_cheeze();
+	printf("f cheeze\n");
+	inf_free();
+	printf("f inf\n");
+	fflush(stdout);
+	fflush(stderr);
+	sync();
+	printf("before exit\n");
+	exit(1);
+}
+
+void * thread_test(void *){
+	vec_request *req=NULL;
+	while((req=get_trace_vectored_request())){
+		assign_vectored_req(req);
+	}
+	return NULL;
+}
+
+pthread_t thr; 
+int main(int argc,char* argv[]){
+	struct sigaction sa;
+	setbuf(stdout, NULL);
+	setbuf(stderr, NULL);
+	sa.sa_handler = log_print;
+	sigaction(SIGINT, &sa, NULL);
+	printf("signal add!\n");
+
+	inf_init(1,0,argc,argv, false);
+
+	init_trace_cheeze();
+
+	pthread_create(&thr, NULL, thread_test, NULL);
+	pthread_join(thr, NULL);
+
+	free_trace_cheeze();
+	inf_free();
+	return 0;
+}
+
