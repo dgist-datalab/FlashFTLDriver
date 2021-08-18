39cf75b9ce9845aeffb9e05567c68f7c39a65a78
kukania <angkswnstn@gmail.com>
2021-08-19 01:46:18 +0900

    temp commit

--- blockdev_making_all.sh
+++ blockdev_making_all.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+mkdir -f ./result
+rm ./result/*_driver
+
 make TARGET_ALGO=DFTL cheeze_block_driver -j
 mv cheeze_block_driver ./result/cheeze_dftl_driver
+make clean
+
+make TARGET_ALGO=Page_ftl cheeze_block_driver -j
+mv cheeze_block_driver ./result/cheeze_page_driver
 make clean
 
 make TARGET_ALGO=lsmftl cheeze_block_driver -j
 mv cheeze_block_driver ./result/cheeze_lsmftl_driver
 make clean
 
-make TARGET_ALGO=Page_ftl cheeze_block_driver -j
-mv cheeze_block_driver ./result/cheeze_page_driver
-make clean
+ls -al ./result
