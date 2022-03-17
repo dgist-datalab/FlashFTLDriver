23d7104c2042a625e5f8ac29a0aa11c16cff3d0d
kukania <angkswnstn@gmail.com>
2022-03-17 13:45:34 +0900

    fix error

--- algorithm/layeredLSM/level.cpp
+++ algorithm/layeredLSM/level.cpp
@@ -27,7 +27,6 @@ void level_get_compaction_target(level *lev, uint32_t run_num, run*** target){
 	else if(run_num==2){
 		uint32_t first_idx=UINT32_MAX;
 		uint32_t second_idx=UINT32_MAX;
-		float target_ratio=0;
 		
 		float target_ratio=-1.0f;
 		for(uint32_t round=0; round<run_num; round++){
