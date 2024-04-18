67f0831ed179f45d5087c6fab0f2d4de9408f7c8
kukania <angkswnstn@gmail.com>
2024-04-18 17:28:00 +0900

    tt

---
+++ synthetic_making_all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+mkdir -f ./result
+rm ./result/*_driver
+
+make TARGET_ALGO=DFTL driver -j
+mv driver ./result/dftl_driver
+make clean
+
+make TARGET_ALGO=Page_ftl driver -j
+mv driver ./result/page_driver
+make clean
+
+make TARGET_ALGO=layeredLSM driver -j
+mv driver ./result/lsmftl_driver
+make clean
+
+make TARGET_ALGO=leaFTL driver -j
+mv driver ./result/leaftl_driver
+make clean
+
+ls -al ./result
